4690cc2d2fffd5f39b6e9f34343cba0502ea123c
EvelynOliveira1200 <evelynoliveira.tds1.senai@gmail.com>
2025-04-08 11:28:28 -0300

    feat: update structure

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -6,7 +6,7 @@ const getAllPosts = async (req, res) => {
         const posts = await postModels.getPosts(description);
         res.status(200).json(posts);
     } catch (error) {
-        res.status(500).json({ mensagem: "Erro ao buscar posts" });
+        res.status(500).json({ message: "Erro ao buscar posts" });
     }
 };
 
@@ -14,11 +14,11 @@ const getPost = async (req, res) => {
     try {
         const post = await postModels.getPostById(req.params.id);
         if (!post) {
-            return res.status(404).json({ mensagem: "Post não encontrado" });
+            return res.status(404).json({ message: "Post não encontrado" });
         }
         res.status(200).json(post);
     } catch (error) {
-        res.status(500).json({ mensagem: "Erro ao buscar post" });
+        res.status(500).json({ message: "Erro ao buscar post" });
     }
 };
 
@@ -28,7 +28,7 @@ const createPost = async (req, res) => {
         const newPost = await postModels.createPost(user_id, image, description, add_person, localization);
         res.status(201).json(newPost);
     } catch (error) {
-        res.status(500).json({ mensagem: "Erro ao criar post" });
+        res.status(500).json({ message: "Erro ao criar post" });
     }
 };
 
@@ -38,20 +38,20 @@ const updatePost = async (req, res) => {
         const { description, add_person } = req.body;
         const updatedPost = await postModels.updatePost(id, description, add_person);
         if (!updatedPost) {
-            return res.status(404).json({ mensagem: "Post não encontrado" });
+            return res.status(404).json({ message: "Post não encontrado" });
         }
         res.status(200).json(updatedPost);
     } catch (error) {
-        res.status(500).json({ mensagem: "Erro ao atualizar post" });
+        res.status(500).json({ message: "Erro ao atualizar post" });
     }
 };
 
 const deletePost = async (req, res) => {
     try {
         const deletedPost = await postModels.deletePost(req.params.id);
-        res.status(200).json({ mensagem: "Post deletado com sucesso", post: deletedPost });
+        res.status(200).json({ message: "Post deletado com sucesso", post: deletedPost });
     } catch (error) {
-        res.status(404).json({ mensagem: "Post não encontrado" });
+        res.status(404).json({ message: "Post não encontrado" });
     }
 };
 
--- src/models/postModels.js
+++ src/models/postModels.js
@@ -1,33 +1,47 @@
 const pool = require("../config/database");
 
-const getPosts = async ( description ) => {
+const getPosts = async (description) => {
     if (!description) {
-        const result = await pool.query("SELECT posts.*, users.name AS usuario FROM posts LEFT JOIN users ON posts.user_id = users.id");
+        const result = await pool.query(`SELECT posts.*, users.name AS usuario 
+            FROM posts 
+            LEFT JOIN users 
+            ON posts.user_id = users.id`);
         return result.rows;
     } else {
-        const result = await pool.query("SELECT posts.*, users.name AS usuario FROM posts LEFT JOIN users ON posts.user_id = users.id WHERE posts.description ILIKE $1", [`%${description}%`]
-        );
+        const result = await pool.query(`SELECT posts.*, users.name AS usuario 
+            FROM posts 
+            LEFT JOIN users ON posts.user_id = users.id 
+            WHERE posts.description ILIKE $1`, [`%${description}%`]);
         return result.rows;
     }
 };
 
 const getPostById = async (id) => {
-    const result = await pool.query("SELECT posts.id, posts.image, posts.description, posts.add_person, posts.localization, users.name AS usuario FROM posts LEFT JOIN users ON posts.user_id = users.id WHERE posts.id = $1", [id]);
+    const result = await pool.query(`SELECT posts.id, posts.image, posts.description, posts.add_person, posts.localization, 
+        users.name AS usuario 
+        FROM posts 
+        LEFT JOIN users 
+        ON posts.user_id = users.id 
+        WHERE posts.id = $1`, [id]);
     return result.rows[0];
 };
 
 const createPost = async (user_id, image, description, add_person, localization) => {
-    const result = await pool.query("INSERT INTO posts (user_id, image, description, add_person, localization) VALUES ($1, $2, $3, $4, $5) RETURNING *", [user_id, image, description, add_person, localization]);
+    const result = await pool.query(`INSERT INTO posts (user_id, image, description, add_person, localization) 
+        VALUES ($1, $2, $3, $4, $5) RETURNING *`, [user_id, image, description, add_person, localization]);
     return result.rows[0];
 };
 
 const updatePost = async (id, description, add_person) => {
-    const result = await pool.query("UPDATE posts SET description = $1, add_person = $2 WHERE id = $3 RETURNING *", [description, add_person, id]);
+    const result = await pool.query(`UPDATE posts 
+        SET description = $1, add_person = $2 
+        WHERE id = $3 RETURNING *`, [description, add_person, id]);
     return result.rows[0];
 };
 
 const deletePost = async (id) => {
-    const result = await pool.query("DELETE FROM posts WHERE id = $1 RETURNING *", [id]);
+    const result = await pool.query(`DELETE FROM posts 
+        WHERE id = $1 RETURNING *`, [id]);
     if (result.rowCount === 0) {
         throw new Error("Post not found");
     }
